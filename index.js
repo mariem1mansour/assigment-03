ee4f7aa86ebfc6bfd97c01e9ed35ddf987588789
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-02-03 22:45:50 +0100

    using React.createElement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// using React.createElement
+
 const Title = React.createElement("div", { class: "title" }, [
   React.createElement("h1", {}, "Hello H1 Tag"),
   React.createElement("h2", {}, "Hello H2 Tag"),
@@ -9,12 +11,14 @@ const Title = React.createElement("div", { class: "title" }, [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(Title);
 
-const jsxTitle = (
-  <div>
-    <h1>Hello from jsx H1 Tag</h1>
-    <h2>Hello from jsx H2 Tag</h2>
-    <h3>Hello from jsx H3 Tag</h3>
-  </div>
-);
+//same element using jsx
 
-const jsxFunctional = () => <jsxTitle />;
+// const jsxTitle = (
+//   <div>
+//     <h1>Hello from jsx H1 Tag</h1>
+//     <h2>Hello from jsx H2 Tag</h2>
+//     <h3>Hello from jsx H3 Tag</h3>
+//   </div>
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxTitle);
